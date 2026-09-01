52d72becd94d6981b0377ab76926fecd01f37fcb
Shmuel Shif <shmulikshif@gmail.com>
2026-09-01 17:28:31 +0300

    cs

---
+++ printer-service/start-print-service.bat
@@ -0,0 +1,10 @@
+@echo off
+title LECHAIM Print Service
+cd /d "%~dp0"
+echo LECHAIM Print Service
+echo Do not close this window.
+echo.
+npm.cmd start
+echo.
+echo The print service stopped.
+pause
